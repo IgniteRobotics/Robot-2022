fed91344481899a1f63a73ab126ec4dcc4e450bd
Josh Reddick <y0shi@github.com>
2022-03-25 20:26:19 -0400

    Servo go brrrrrr

--- src/main/java/frc/robot/commands/shooter/SetHoodPosition.java
+++ src/main/java/frc/robot/commands/shooter/SetHoodPosition.java
@@ -25,7 +25,7 @@ public class SetHoodPosition extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    hood.setAngle(positionSupplier.get());
+    hood.setPosition(positionSupplier.get());
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -41,6 +41,6 @@ public class SetHoodPosition extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return hood.isInPosition();
   }
 }
--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -9,29 +9,102 @@ import com.igniterobotics.robotbase.reporting.ReportingNumber;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Hood extends SubsystemBase {
   private ReportingNumber hoodAngleReporter = new ReportingNumber("Hood Set Angle", ReportingLevel.COMPETITON);
 
-  private Servo hoodServo = new Servo(3);
+  private Servo hoodServo = new LinearServo(3, 50, 32);
+  private double targetPosition = 0;
 
   /** Creates a new Hood. */
   public Hood() {
-    hoodServo.setBounds(2, 1.5, 1.5, 1.5, 1);
+  
   }
 
   public void setPosition(double position) {
-
-  }
-
-  public void setAngle(double degrees) {
-    degrees = MathUtil.clamp(degrees, 0, 360);
-    hoodServo.setAngle(degrees);
-  }
+    hoodServo.setPosition(position);
+    targetPosition = position;
+ }
 
   @Override
   public void periodic() {
-    hoodAngleReporter.set(hoodServo.getAngle());
+    hoodAngleReporter.set(getPosition());
   }
+
+  public double getPosition() {
+    return hoodServo.getPosition();
+  }
+
+  public boolean isInPosition(){
+    return hoodServo.getPosition() == targetPosition;
+  }
+
+  class LinearServo extends Servo
+  {
+      double m_speed;
+      double m_length;
+      double setPos;
+      double curPos;
+
+      /**
+       * Parameters for L16-R Actuonix Linear Actuators
+       *
+       * @param channel PWM channel used to control the servo
+       * @param length  max length of the servo [mm]
+       * @param speed   max speed of the servo [mm/second]
+       */
+      public LinearServo(int channel, int length, int speed)
+      {
+          super(channel);
+          setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
+          m_length = length;
+          m_speed = speed;
+          
+      }
+
+      /**
+       * Run this method in any periodic function to update the position estimation of
+       * your servo
+       *
+       * @param setpoint the target position of the servo [mm]
+       */
+      public void setPosition(double setpoint)
+      {
+          setPos = MathUtil.clamp(setpoint, 0, m_length);
+          setSpeed((setPos / m_length * 2) - 1);
+      }
+
+      double lastTime = 0;
+
+      /**
+       * Run this method in any periodic function to update the position estimation of
+       * your servo
+       */
+      public void updateCurPos()
+      {
+          double dt = Timer.getFPGATimestamp() - lastTime;
+          if (curPos > setPos + m_speed * dt) {
+              curPos -= m_speed * dt;
+          } else if (curPos < setPos - m_speed * dt) {
+              curPos += m_speed * dt;
+          } else {
+              curPos = setPos;
+          }
+      }
+
+      /**
+       * Current position of the servo, must be calling {@link #updateCurPos()
+       * updateCurPos()} periodically
+       *
+       * @return Servo Position [mm]
+       */
+      public double getPosition()
+      {
+          return curPos;
+      }
+  }
+
+
 }
