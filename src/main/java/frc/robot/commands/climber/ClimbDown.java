c0cd3f255179e87a8e6d2f79f93619e26d136aec
Coby-Brown <sam.b@igniterobotics.com>
2022-04-18 13:51:55 -0400

    Added Comments to ClimbDown.java

--- src/main/java/frc/robot/commands/climber/ClimbDown.java
+++ src/main/java/frc/robot/commands/climber/ClimbDown.java
@@ -2,36 +2,40 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+//Makes this file recognized as commands used by the climber
 package frc.robot.commands.climber;
 
+//Imports WPIlibs command base and the Climber subsystem
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Climber;
 
+//Creates a new command, ClimbDown, which uses other commands located in the Climber subsystem, and is used by RobotContainer.java. TODO Ask if we need this whole file, as RobotContainer.java imports the climber subsystem anyway.
 public class ClimbDown extends CommandBase {
   private Climber climb;
-  /** Creates a new ClimbUp. */
+  /** Creates a new ClimbDown. */
   public ClimbDown(Climber climb) {
     // Use addRequirements() here to declare subsystem dependencies.
+    //Recognizes the climb used on line 16 is the same as the climb used on line 14
     this.climb = climb;
   }
 
-  // Called when the command is initially scheduled.
+  //Called when the command is initially scheduled, It doesn't seem we use this. TODO Ask about the purpose for this command, and if we need it.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
+  //Called every time the scheduler runs while the command is scheduled, tells the climb to go down.
   @Override
   public void execute() {
     climb.goDown();
   }
 
-  // Called once the command ends or is interrupted.
+  //Called once the command ends or is interrupted, tells the climb to stop.
   @Override
   public void end(boolean interrupted) {
     climb.stop();
   }
 
-  // Returns true when the command should end.
+  //Used to end the command, Checks if the climb subsystems limit has been met, and if it has been met, returns true.
   @Override
   public boolean isFinished() {
     return climb.isClimbLimitMet();
